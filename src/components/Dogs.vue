a17d3613f54b0d51bdb6cc1a3a95076777a7a29f
wabens <waltkbenson@gmail.com>
2019-08-07 13:33:50 -0500

    playing with mounted hook and added style

--- src/components/Dogs.vue
+++ src/components/Dogs.vue
@@ -30,13 +30,38 @@ export default {
       name: ''
     }
   },
-
+    mounted(){
+    this.dogs=[{name: 'Juno'}, {name: 'Bob'}];
+  },
   methods: {
     addDog(){
       const exists = this.dogs.some(dog => dog.name === this.name);
       if (!exists) this.dogs = sortDogs(this.dogs.concat({name: this.name}));
       this.name = '';
-    }
+    },
+
   }
 }
 </script>
+
+<style scoped>
+button, input {
+  border: solid gray 1px;
+  border-radius: 4px;
+  padding: 4px;
+}
+input {
+  margin: 0 10px;
+}
+label {
+  font-weight: bold;
+}
+table {
+  border-collapse: collapse;
+  margin-bottom: 10px;
+}
+td, th {
+  border: solid gray 1px;
+  padding: 5px;
+}
+</style>
